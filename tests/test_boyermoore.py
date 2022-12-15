236c2b20fc3ea21136620bf05584dbde45c27da6
Erik Nyquist <eknyquist@gmail.com>
2022-12-14 22:46:55 -0800

    Improve code coverage, add coverage script

--- tests/test_boyermoore.py
+++ tests/test_boyermoore.py
@@ -8,14 +8,23 @@ from tests.common import make_big_bytes, make_big_file
 
 TEST_STRINGS = [
     "howdy ho",
+    "AbCdEfG",
+    "aqaqaq",
+    "zqxzqx",
     "Loprem Ipsum",
     "hello, world!",
-    "Hello, World!",
+    "1 ; 'D,>/?0}[_[@6&8Kd __=-=",
     "IIJJIIJJKKKKKKKK",
+    "  ",
+    "AAAAA",
+    "AAAAAA",
+    "AAAAAAA",
+    "Hello World, This is a Typical string of words And Stuff!!..?u",
+    "PPPPPPPPPPPPPPPpppppppppppp",
     "hello this is a test string and I need to make it kinda long so",
-    "AaBbCcDdEeFf{};';GgHhIiJjKkLlMmNn(*98Jaahgoi78^&*6%6745tUuVv",
     "कखगघ ङचछजझञ टठडढणतथदधन",
     "ճմնշոչպ ջռսվ տրց",
+    "dճմddնշgdtrhոչ9898պ seeջռսվ :;':տրց",
     "⅓ ⅔ ⅕ ⅖⅗ ⅘ ⅙⅚⅛ ⅜ ⅝⅞ ⅟ Ⅰ Ⅱ Ⅲ ⅣⅤⅥ Ⅶ Ⅷ Ⅸ Ⅹ ⅪⅫ Ⅼ ⅭⅮ Ⅿ ⅰⅱⅲ ⅳ ⅴ ⅵⅶⅷ ⅸ ⅹ ⅺⅻ"
 ]
 
@@ -24,9 +33,8 @@ TEST_OFFSETS = [
     [1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072],
     [1000, 5000, 20000],
     [131072],
-    [0, 500],
-    [0, 1024],
-    [0, 1024 * 1024]
+    [1, 221, 442, 663, 884, 1095, 1306, 1507, 1708],
+    [0, 1024 * 1024],
 ]
 
 TEST_DATA = {s: [o for o in TEST_OFFSETS] for s in TEST_STRINGS}
@@ -62,6 +70,55 @@ class TestBoyerMoore(unittest.TestCase):
                 test_string = make_big_bytes(pattern.encode(), expected_offsets)
                 actual_offsets = search_string_pp(pp_data, test_string, greedy=False)
                 self.assertEqual(actual_offsets, [expected_offsets[0]])
+
+    def test_search_string_empty_pattern(self):
+        test_string = b'hhhhhhhhhh'
+        offsets = search_string('', test_string)
+        self.assertEqual(offsets, [])
+
+    def test_search_string_perfectfit_pattern(self):
+        test_string = b'abcdabcdabcdabcd'
+        offsets = search_string('abcd', test_string)
+        self.assertEqual(offsets, [0, 4, 8, 12])
+
+    def test_search_string_partialmatch_pattern(self):
+        test_string = b'abcqabcgabcdabdj'
+        offsets = search_string('abcd', test_string)
+        self.assertEqual(offsets, [8])
+
+    def test_search_string_nonexistent_pattern(self):
+        test_string = b'hhhhhhhhhh'
+        offsets = search_string('y', test_string)
+        self.assertEqual(offsets, [])
+
+    def test_search_string_1byte_pattern(self):
+        test_string = b'hhqhhhhhhh'
+        offsets = search_string('q', test_string)
+        self.assertEqual(offsets, [2])
+
+    def test_search_file_empty_pattern(self):
+        filename = "testfile.txt"
+        with open(filename, 'wb') as fh:
+            fh.write(b'hhhhhhhhhh')
+
+        offsets = search_file('', filename)
+        self.assertEqual(offsets, [])
+
+    def test_search_file_nonexistent_pattern(self):
+        filename = "testfile.txt"
+        with open(filename, 'wb') as fh:
+            fh.write(b'hhhhhhhhhh')
+
+        offsets = search_file('p', filename)
+        self.assertEqual(offsets, [])
+
+    def test_search_file_1byte_pattern(self):
+        filename = "testfile.txt"
+        with open(filename, 'wb') as fh:
+            fh.write(b'hhqhhhhhhh')
+
+        offsets = search_file('q', filename)
+        self.assertEqual(offsets, [2])
 
     def test_search_file_greedy(self):
         count = 0
@@ -132,3 +189,10 @@ class TestBoyerMoore(unittest.TestCase):
                 self.assertEqual(actual_offsets, [expected_offsets[0]])
 
                 os.remove(filename)
+
+    def test_preprocess_invalid_type(self):
+        self.assertRaises(ValueError, preprocess, 5.5)
+        self.assertRaises(ValueError, preprocess, {})
+        self.assertRaises(ValueError, preprocess, [])
+        self.assertRaises(ValueError, preprocess, (1,1))
+        self.assertRaises(ValueError, preprocess, True)
