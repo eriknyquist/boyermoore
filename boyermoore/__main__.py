e9cb745b3d5149177c3ea3ad8fb0416e02ae673f
Erik Nyquist <eknyquist@gmail.com>
2022-12-16 20:32:30 -0800

    Remove unneeded files

--- boyermoore/__main__.py
+++
@@ -1,7 +0,0 @@
-from boyermoore import search_file
-
-def main():
-    print(search_file("Hello नमस्ते Привет こんにちは", "big_file.txt"))
-
-if __name__ == "__main__":
-    main()
--- scripts/searchfiletest.py
+++
@@ -1,8 +0,0 @@
-#from boyermoore import search_file
-import boyermoore
-
-def main():
-    print(boyermoore.search_file("Hello नमस्ते Привет こんにちは", "big_file.txt"))
-
-if __name__ == "__main__":
-    main()
